2a6975edc11d6bb56965a8364c7db21f3e988ff6
TheVictorDevX <thevictordevx@gmail.com>
2025-08-28 10:47:22 +0700

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,55 +12,58 @@ const Hero = () => {
   };
 
   return (
-    <section className="w-full flex flex-col justify-center items-center gap-y-1.5 p-8">
+    <section className="w-full flex flex-col lg:flex-row justify-center items-center py-8 lg:gap-x-25">
       {/* Profile Picture */}
-      <div className="hover:scale-110 hover:m-2 transition-all bg-gradient-to-r from-gradient-1 to-gradient-2 rounded-full w-60 p-1.25">
+      <div className="lg:order-last hover:scale-110 hover:m-2 transition-all bg-gradient-to-r from-gradient-1 to-gradient-2 rounded-full w-60 p-1.25">
         <img src={profilePic} alt="Profile Picture" className="rounded-full" />
       </div>
 
-      {/* Greeting Title */}
-      <h1 className="hover:scale-110 hover:m-2 transition-all font-bold text-2xl text-transparent bg-clip-text bg-gradient-to-r from-gradient-1 to-gradient-2">
-        Hi, I'm Lim Chanvirakyuth
-      </h1>
+      {/* Second Part of Hero Section */}
+      <div className="flex flex-col justify-center items-center lg:items-start gap-y-1.5">
+        {/* Greeting Title */}
+        <h1 className="lg:text-left hover:scale-110 hover:m-2 transition-all font-bold text-2xl text-transparent bg-clip-text bg-gradient-to-r from-gradient-1 to-gradient-2">
+          Hi, I'm Lim Chanvirakyuth
+        </h1>
 
-      {/* Greeting Description */}
-      <p className="text-center leading-5">
-        I build responsive and beautiful websites.
-        <br />
-        My goal is to create seamless digital experiences
-        <br />
-        that are a joy for people to use.
-      </p>
+        {/* Greeting Description */}
+        <p className="lg:text-left text-center leading-5">
+          I build responsive and beautiful websites.
+          <br />
+          My goal is to create seamless digital experiences
+          <br />
+          that are a joy for people to use.
+        </p>
 
-      {/* Group Buttons */}
-      <div className="flex justify-center items-center gap-x-1 mt-1">
-        {/* GitHub Button */}
+        {/* Group Buttons */}
+        <div className="lg:mt-5 flex justify-center items-center gap-x-1 mt-1">
+          {/* GitHub Button */}
+          <a
+            className="hover:scale-110 hover:m-2 transition-all h-12 p-3 text-black font-bold bg-gradient-to-r from-gradient-1 to-gradient-2 rounded-full flex justify-center items-center"
+            href="https://github.com/TheVictorDevX"
+            target="_blank"
+          >
+            <FaGithub className="text-3xl mr-2" /> GitHub
+          </a>
+          {/* Telegram Button */}
+          <a
+            className="hover:scale-110 hover:m-2 transition-all h-12 p-3 text-black font-bold bg-gradient-to-r from-gradient-1 to-gradient-2 rounded-full flex justify-center items-center"
+            href="https://t.me/TaiKaizenToo"
+            target="_blank"
+          >
+            <FaTelegram className="text-3xl mr-2" /> Telegram
+          </a>
+        </div>
+
+        {/* Download CV Button */}
         <a
-          className="hover:scale-110 hover:m-2 transition-all h-12 p-3 text-black font-bold bg-gradient-to-r from-gradient-1 to-gradient-2 rounded-full flex justify-center items-center"
-          href="https://github.com/TheVictorDevX"
+          className="lg:w-63 hover:scale-110 hover:m-2 transition-all h-12 p-3 mt-0.5 text-black font-bold bg-gradient-to-r from-gradient-1 to-gradient-2 rounded-full flex justify-center items-center"
+          href={CV}
           target="_blank"
+          download
         >
-          <FaGithub className="text-3xl mr-2" /> GitHub
-        </a>
-        {/* Telegram Button */}
-        <a
-          className="hover:scale-110 hover:m-2 transition-all h-12 p-3 text-black font-bold bg-gradient-to-r from-gradient-1 to-gradient-2 rounded-full flex justify-center items-center"
-          href="https://t.me/TaiKaizenToo"
-          target="_blank"
-        >
-          <FaTelegram className="text-3xl mr-2" /> Telegram
+          <FaFileDownload className="text-3xl mr-2" /> Download CV
         </a>
       </div>
-
-      {/* Download CV Button */}
-      <a
-        className="hover:scale-110 hover:m-2 transition-all h-12 p-3 mt-0.5 text-black font-bold bg-gradient-to-r from-gradient-1 to-gradient-2 rounded-full flex justify-center items-center"
-        href={CV}
-        target="_blank"
-        download
-      >
-        <FaFileDownload className="text-3xl mr-2" /> Download CV
-      </a>
     </section>
   );
 };
