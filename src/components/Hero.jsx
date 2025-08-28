a15a944ba1dca4e2ac6e166b2c622df98cdb65c8
TheVictorDevX <thevictordevx@gmail.com>
2025-08-28 09:14:30 +0700

    Make Hero.jsx Responsive Better

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -27,27 +27,27 @@ const Hero = () => {
         <div className="w-full my-3 text-black font-bold flex sm:justify-center md:justify-center lg:justify-start lg:mt-5 items-center gap-x-5 lg:gap-x-3">
           <button
             onClick={onGitHubClick}
-            className="sm:h-15 sm:w-75 md:w-75 lg:w-50 hover:cursor-pointer hover:scale-110 transition-all bg-gradient-to-r from-gradient-1 to-gradient-2 text-2xl h-75/100 w-60 rounded-full flex justify-center items-center"
+            className="sm:h-15 sm:w-75 md:w-75 lg:w-50 hover:cursor-pointer hover:scale-110 transition-transform bg-gradient-to-r from-gradient-1 to-gradient-2 text-2xl h-75/100 w-60 rounded-full flex justify-center items-center"
           >
             <FaGithub className="text-4xl mr-3" />
             GitHub
           </button>
           <button
             onClick={onTelegramClick}
-            className="sm:h-15 sm:w-75 md:w-75 lg:w-50 hover:cursor-pointer hover:scale-110 transition-all bg-gradient-to-r from-gradient-1 to-gradient-2 text-2xl h-75/100 w-60 rounded-full flex justify-center items-center"
+            className="sm:h-15 sm:w-75 md:w-75 lg:w-50 hover:cursor-pointer hover:scale-110 transition-transform bg-gradient-to-r from-gradient-1 to-gradient-2 text-2xl h-75/100 w-60 rounded-full flex justify-center items-center"
           >
             <FaTelegram className="text-4xl mr-3" />
             Telegram
           </button>
         </div>
         <div className="flex justify-center items-center lg:justify-start">
-          <a href={CV} download="MyExampleDoc" target='_blank' className="sm:w-120 md:w-120 lg:w-103 hover:cursor-pointer hover:scale-110 transition-all text-black font-bold bg-gradient-to-r from-gradient-1 to-gradient-2 text-2xl h-15 w-full rounded-full flex justify-center items-center">
+          <a href={CV} download="MyExampleDoc" target='_blank' className="sm:w-120 md:w-120 lg:w-103 hover:cursor-pointer hover:scale-110 transition-transform text-black font-bold bg-gradient-to-r from-gradient-1 to-gradient-2 text-2xl h-15 w-full rounded-full flex justify-center items-center">
           <FaFileDownload className="text-4xl mr-3" />
           Download CV
           </a>
         </div>
       </div>
-      <div className="sm:order-first lg:order-last sm:flex sm:justify-center sm:items-center hover:scale-110 transition-all">
+      <div className="sm:order-first lg:order-last sm:flex sm:justify-center sm:items-center hover:scale-110 transition-transform">
         <div className="rounded-full bg-gradient-to-r from-gradient-1 to-gradient-2 p-1.25">
           <img
             src={profilePic}
